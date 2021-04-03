daa0c86a37ce305cf796c3a7716832e6e269f64c
edgarakopyan <edgar1996.96@gmail.com>
2021-04-03 22:27:49 +0200

    Some pre-processing of Passenger ID and Cabin Letter and Number

--- April Competition.py
+++ April Competition.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import os
+import numpy as np
 import sklearn.preprocessing as skp
 
 # Check if directory is correct
@@ -9,6 +10,11 @@ os.getcwd()
 train_df = pd.read_csv("train.csv")
 test_df = pd.read_csv("test.csv")
 sample_submission = pd.read_csv("sample_submission.csv")
+
+# Get indeces of test values and delete Passenger ID columns in both datasets
+Test_IDs = test_df['PassengerId']
+test_df = test_df.drop(['PassengerId'], axis = 1)
+train_df = train_df.drop(['PassengerId'], axis = 1)
 
 # Delete names column
 test_df = test_df.drop(['Name'], axis = 1)
@@ -25,5 +31,17 @@ test_df = pd.concat([test_df, pd.get_dummies(train_df['Sex'])], axis = 1)
 test_df = test_df.drop([ 'Sex','male'], axis = 1)
 train_df = train_df.drop(['Sex','male'], axis = 1)
 
+# Create a new column with cabin letter only and one with cabin number only
+
+train_df['Cabin_Letter'] = ''
+train_df['Cabin_Number'] = ''
+
+for i in train_df[train_df['Cabin'].notna()].index:
+        train_df['Cabin_Letter'][i] = train_df['Cabin'][i][0]
+        train_df['Cabin_Number'][i] = train_df['Cabin'][i][1:]
+
+for i in train_df[train_df['Cabin'].isna()].index:
+        train_df['Cabin_Letter'][i] = np.nan
+        train_df['Cabin_Number'][i] = np.nan
 
 
