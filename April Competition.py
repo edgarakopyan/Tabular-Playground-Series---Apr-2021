8f975f5d0a9b35662a91c87b5cd4f4aa91a8ae03
edgarakopyan <edgar1996.96@gmail.com>
2021-04-03 20:37:18 +0200

    Some pre-processing of Sex and Name variables

--- April Competition.py
+++ April Competition.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import os
+import sklearn.preprocessing as skp
 
 # Check if directory is correct
 os.getcwd()
@@ -10,3 +11,19 @@ test_df = pd.read_csv("test.csv")
 sample_submission = pd.read_csv("sample_submission.csv")
 
 # Delete names column
+test_df = test_df.drop(['Name'], axis = 1)
+train_df = train_df.drop(['Name'], axis = 1)
+
+# encode sex column. Before we do that, we check that there are no missing values. We then drop those columns
+
+test_df[test_df['Sex'].isna()]
+train_df[train_df['Sex'].isna()]
+
+train_df = pd.concat([train_df,pd.get_dummies(train_df['Sex'])], axis = 1)
+test_df = pd.concat([test_df, pd.get_dummies(train_df['Sex'])], axis = 1)
+
+test_df = test_df.drop([ 'Sex','male'], axis = 1)
+train_df = train_df.drop(['Sex','male'], axis = 1)
+
+
+
