3ad5d865535b58e13d1ab93f5e8e8ec0ae9110f8
edgarakopyan <edgar1996.96@gmail.com>
2021-04-05 14:29:55 +0200

    Rudimentary Logistic and Random tree models

--- April Competition.py
+++ April Competition.py
@@ -2,6 +2,10 @@ import pandas as pd
 import os
 import numpy as np
 import sklearn.preprocessing as skp
+import sklearn.linear_model
+import sklearn.ensemble
+
+pd.options.mode.chained_assignment = None
 
 # Check if directory is correct
 os.getcwd()
@@ -14,7 +18,6 @@ sample_submission = pd.read_csv("sample_submission.csv")
 # Get indeces of test values and delete Passenger ID columns in both datasets
 Test_IDs = test_df['PassengerId']
 test_df = test_df.drop(['PassengerId'], axis = 1)
-train_df = train_df.drop(['PassengerId'], axis = 1)
 
 # Delete names column
 test_df = test_df.drop(['Name'], axis = 1)
@@ -44,4 +47,32 @@ for i in train_df[train_df['Cabin'].isna()].index:
         train_df['Cabin_Letter'][i] = np.nan
         train_df['Cabin_Number'][i] = np.nan
 
+
+test_df['Cabin_Letter'] = ''
+test_df['Cabin_Number'] = ''
+
+for i in test_df[test_df['Cabin'].notna()].index:
+        test_df['Cabin_Letter'][i] = test_df['Cabin'][i][0]
+        test_df['Cabin_Number'][i] = test_df['Cabin'][i][1:]
+
+for i in test_df[test_df['Cabin'].isna()].index:
+        test_df['Cabin_Letter'][i] = np.nan
+        test_df['Cabin_Number'][i] = np.nan
+
+# Try a rudimentary logistic model
+model = sklearn.linear_model.LogisticRegression().fit(train_df[['SibSp','Pclass','Parch', 'female']], train_df.Survived) # 'Pclass',
+prediction1_df = pd.Series(model.predict(test_df[['SibSp','Pclass','Parch', 'female']]))
+prediction1_df = pd.concat([Test_IDs, prediction1_df], axis = 1)
+prediction1_df = pd.DataFrame(prediction1_df)
+prediction1_df.columns = ['PassengerId', 'Survived']
+prediction1_df.to_csv('prediction1.csv', index = False)
+
+# Try rudimentary Random Forest
+forest1 = sklearn.ensemble.RandomForestClassifier(n_estimators = 125).fit(train_df[['SibSp','Pclass','Parch', 'female']],
+                                                                          train_df.Survived)
+prediction2_df = forest1.predict(test_df[['SibSp','Pclass','Parch', 'female']])
+prediction2_df = pd.DataFrame(prediction2_df)
+prediction2_df = pd.concat([Test_IDs, prediction2_df], axis = 1)
+prediction2_df.columns = ['PassengerId', 'Survived']
+prediction2_df.to_csv('prediction2.csv', index = False)
 
